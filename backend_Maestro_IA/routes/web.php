d9b8123ee6f19d72a022d242d2cb5eb642d3a6c9
Emilio <mor21804@uvg.edu.gt>
2026-07-06 02:31:46 -0600

    fix: forzar drivers locales en runtime

--- backend_Maestro_IA/routes/web.php
+++ backend_Maestro_IA/routes/web.php
@@ -9,16 +9,16 @@ Route::get('/', function () {
 
 Route::get('/run-migrations-xyz', function () {
     try {
-        // Obliga al contenedor de Docker a limpiar el caché de rutas generado en el build
-        Artisan::call('optimize:clear');
-        $clearOutput = Artisan::output();
+        // Forzamos manualmente los drivers a 'file' en tiempo de ejecución
+        // Esto ignora cualquier caché viejo de configuración que tenga el contenedor
+        config(['cache.default' => 'file']);
+        config(['session.driver' => 'file']);
 
-        // Ejecuta las migraciones en Supabase
+        // Ejecutamos las migraciones en Supabase
         Artisan::call('migrate', ['--force' => true]);
         $migrateOutput = Artisan::output();
 
         return '<h3>Proceso Completado</h3>' .
-            '<pre><b>Caché Limpiado:</b><br>' . $clearOutput . '</pre>' .
             '<pre><b>Resultado de la Migración:</b><br>' . $migrateOutput . '</pre>';
     } catch (\Exception $e) {
         return '<h3>Error al migrar:</h3><pre>' . $e->getMessage() . '</pre>';
