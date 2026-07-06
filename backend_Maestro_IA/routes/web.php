3bc97c9032bb6a9898b68dc1b194bfbd17fb912a
Emilio <mor21804@uvg.edu.gt>
2026-07-06 02:38:27 -0600

    Regresando a la normalidad todo

--- backend_Maestro_IA/routes/web.php
+++ backend_Maestro_IA/routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/run-migrations-xyz', function () {
-    try {
-        // Forzamos manualmente los drivers a 'file' en tiempo de ejecución
-        // Esto ignora cualquier caché viejo de configuración que tenga el contenedor
-        config(['cache.default' => 'file']);
-        config(['session.driver' => 'file']);
-
-        // Ejecutamos las migraciones en Supabase
-        Artisan::call('migrate', ['--force' => true]);
-        $migrateOutput = Artisan::output();
-
-        return '<h3>Proceso Completado</h3>' .
-            '<pre><b>Resultado de la Migración:</b><br>' . $migrateOutput . '</pre>';
-    } catch (\Exception $e) {
-        return '<h3>Error al migrar:</h3><pre>' . $e->getMessage() . '</pre>';
-    }
 });
