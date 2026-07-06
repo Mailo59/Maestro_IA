6324a6918e6c2fbd1ecf639efbb3c2a2c9443e04
Emilio <mor21804@uvg.edu.gt>
2026-07-06 01:50:18 -0600

    feat: ruta temporal para migraciones

--- backend_Maestro_IA/routes/web.php
+++ backend_Maestro_IA/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migrations-xyz', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Migraciones ejecutadas con éxito: ' . Artisan::output();
+    } catch (\Exception $e) {
+        return 'Error al migrar: ' . $e->getMessage();
+    }
 });
